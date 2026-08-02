cafe3fa6d8a6170f494f97a89ab7f81848906c74
Muskan Jaiswal <muskanjaiswal1826@gmail.com>
2026-08-02 05:43:05 +0530

    replace the local host with the backend render links

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -44,7 +44,7 @@ import {
 } from "recharts";
 import AddTransactionModal from "../components/Add";
 
-const API_BASE = "http://localhost:4000/api";
+const API_BASE = "https://fintrack-frontend-zpje.onrender.com/api";
 
 const getAuthHeader = () => {
   const token =
